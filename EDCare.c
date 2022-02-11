058e15bc077839942e868e522aff59720caff010
MauricioPimenta <mauriciobpimenta@gmail.com>
2022-02-11 19:13:44 -0300

    Problemas na ListaCuidadores resolvidos até o momento. Iniciando modificações no programa principal.

---
+++ EDCare.c
@@ -0,0 +1,48 @@
+/*----------------------------------------------------------------------------------------------
+ * EDCare.c
+ * Projeto de um Sistema simplificado de controle de saúde de 
+ * idosos utilizando TADs (Tipos Abstratos de Dados) em C.
+ * 
+ * 
+ * 
+ * 
+ * Autores: Maurício Bittencourt Pimenta e Douglas Stein
+ * Data: 02/2022
+ ---------------------------------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Idoso.h"
+#include "Cuidador.h"
+#include "ListaAmigos.h"
+#include "ListaCuidadores.h"
+#include "ListaIdoso.h"
+
+int main()
+{
+
+    printf("Criando lista de idosos...\n");
+    ListIdoso *listaIdosos = inicializaListIdoso ( " ../Entrada/apoio.txt " );
+
+    printf("Preenchendo listas de cuidadores...\n");
+    ListCuidador *listaCuidadores = preencheListPlaylist("./Entrada/cuidadores.txt");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
