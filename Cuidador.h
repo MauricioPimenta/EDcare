e12f9167414857dbbacddfbfd3ea39b317023770
douglastein <86307590+douglastein@users.noreply.github.com>
2022-02-14 19:39:43 -0300

    Update Cuidador.h

--- Cuidador.h
+++ Cuidador.h
@@ -3,42 +3,50 @@
 
 typedef struct cuidador Cuidador;
 
-#include "listaMedidas.h"
-
-
-//  Função que cria a estrutura cuidador
+//  Funcao que cria a estrutura cuidador
 //  inputs: nome do cuidador
 //  output: ponteiro para a estrutura Cuidador
-//  pre-condicao: nenhuma
-//  pos-condicao: ponteiro de retorno existe e está alocado
+//  pre-condicao: nome existe
+//  pos-condicao: ponteiro de retorno existe e esta alocado
 Cuidador* criaCuidador(char* nome);
 
-//  Função que libera memória alocada para o cuidador
+//  Funcao que libera memoria alocada para o cuidador
 //  inputs: ponteiro para a estrutura cuidador
 //  output: nenhum
 //  pre-condicao: Cuidador existe
-//  pos-condicao: toda a memória alocada para cuidador foi liberada
+//  pos-condicao: toda a memoria alocada para cuidador foi liberada
 void destroiCuidador(Cuidador* cuidador);
 
-//  Função que retorna o nome do cuidador
+//  Funcao que retorna o nome do cuidador
 //  inputs: ponteiro para a estrutura cuidador
-//  output: ponteiro para char
+//  output: String - ponteiro para char
 //  pre-condicao: cuidador existe
-//  pos-condicao: cuidador nao é modificado
+//  pos-condicao: cuidador nao e modificado
 char* retornaNomeCuidador(Cuidador* cuidador);
 
-//  Função que retorna a lista de medidas do cuidador
+//  Funcao que retorna a latitude do cuidador
 //  inputs: ponteiro para a estrutura cuidador
-//  output: ponteiro para ListMedidas
+//  output: inteiro com dados da latitude
 //  pre-condicao: cuidador existe
-//  pos-condicao: cuidador nao é modificado
-ListMedidas* retornaListMedidas(Cuidador* cuidador);
+//  pos-condicao: cuidador nao e modificado
+int retornaLatitude(Cuidador* cuidador);
+
+//  Funcao que retorna a longitude do cuidador
+//  inputs: ponteiro para a estrutura cuidador
+//  output: inteiro com dados da longitude
+//  pre-condicao: cuidador existe
+//  pos-condicao: cuidador nao e modificado
+int retornaLongitude(Cuidador* cuidador);
+
+void InsereMedidasCuidador(Cuidador* cuidador, int linhaArquivo, char* arquivo);
+
 
 //  Imprime cuidador
 //  inputs: ponteiro para cuidador
 //  output: nenhum
 //  pre-condicao: cuidador existe
-//  pos-condicao: cuidador nao é alterado
+//  pos-condicao: cuidador nao e alterado
 void imprimeCuidador(Cuidador* cuidador);
 
 #endif
+
