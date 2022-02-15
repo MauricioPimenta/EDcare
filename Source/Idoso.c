420c946a553f1ed96b886c70d798b182ce19ccd2
MauricioPimenta <mauriciobpimenta@gmail.com>
2022-02-15 01:01:06 -0300

    Atualização (quase) Definitiva da TAD Idoso

--- Source/Idoso.c
+++ Source/Idoso.c
@@ -1,10 +1,27 @@
+/*
+ * INCLUSAO DE BIBLIOTECAS
+ */
+
+// BIBLIOTECAS PADRÃO
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
+// BIBLIOTECAS DO USUÁRIO
 #include "Idoso.h"
 
+// Constantes da inicialização de idoso
+#define LAT_INIT 0
+#define LONG_INIT 0
+#define TEMP_INIT 0
+#define LAT_INIT 0
 
+#define ERRO_signal -1
+
+/*
+ * Definição da Estrutura do TAD Idoso
+ */
 struct idoso{
   char *nome;
   ListAmigos *amigos;
@@ -12,20 +29,36 @@ struct idoso{
   float latitude;
   float longitude;
   float temperatura;
-  int queda;
+  bool queda;
   int numFebresBaixas;
 };
 
+//  Cria estrutura idoso
+//  inputs: nome do idoso
+//  output: ponteiro para a estrutura Idoso
+//  pre-condicao: não existe
+//  pos-condicao: ponteiro da estrutura alocado pronto para uso
 Idoso* criaIdoso(char *nome){
-  Idoso* idoso = (Idoso*)malloc(sizeof(Idoso));
-  idoso->nome = strdup(nome);
-  idoso->amigos = CriaListAmigos();
-  idoso->cuidadores = inicializaListaCuidador();
-  idoso->latitude = 0;
-  idoso->longitude = 0;
-  idoso->temperatura = 0;
-  idoso->queda = 0;
-  idoso->numFebresBaixas = 0;
+
+	// Retorna nada se o nome não for válido
+	if (nome == NULL){
+		return NULL;
+	}
+	
+	// Reserva espaço na memória e armazena o nome
+	Idoso* idoso = (Idoso*)malloc(sizeof(Idoso));
+	idoso->nome = strdup(nome);
+
+	// Cria as listas de amigos e Cuidadores
+	idoso->amigos = CriaListAmigos();
+	idoso->cuidadores = inicializaListaCuidador();
+
+	// Inicializa os atributos do Idoso com valores padrão (zero)
+	idoso->latitude = LAT_INIT;
+	idoso->longitude = LONG_INIT;
+	idoso->temperatura = TEMP_INIT;
+	idoso->queda = false;
+	idoso->numFebresBaixas = 0;
 
   return idoso;
 }
@@ -42,7 +75,14 @@ Idoso* criaIdoso(char *nome){
 //  pre-condicao: idoso existe
 //  pos-condicao: idoso nao é modificado e nome do idoso disponível
 char *getNomeIdoso(Idoso *idoso){
-  return idoso->nome;
+	
+	// Se idoso não existir, retorna nada
+	if (idoso == NULL){
+		return NULL;
+	}
+	// Se existir, retorna o nome do idoso
+	else
+		return idoso->nome;
 }
 
 //  Retorna Lista de amigos do idoso
@@ -50,38 +90,111 @@ char *getNomeIdoso(Idoso *idoso){
 //  output: ponteiro para estrutura ListAmigos
 //  pre-condicao: idoso precisa existir
 //  pos-condicao: idoso nao é alterado e lista de amigos disponível
+//
 ListAmigos *getListAmigosIdoso(Idoso *idoso){
-  return idoso->amigos;
+
+	// Se idoso não existir, retorna nada
+	if (idoso == NULL){
+		return NULL;
+	}
+	else
+		return idoso->amigos;
 }
 
 //  Retona a lista de cuidadores do idoso
 //  inputs: ponteiro para a estrutura Idoso
-//  output: ponteiro para a estrutura ListCuidadores
+//  output: ponteiro para uma Lista de Cuidadores
 //  pre-condicao: idoso precisa existir
 //  pos-condicao: idoso nao é modificado e estrutura dos cuidadores disponível
 ListCuidador *getListCuidadores(Idoso *idoso){
-  return idoso->cuidadores;
+
+	// Se idoso não existir, retorna nada
+	if (idoso == NULL){
+		return NULL;
+	}
+	else
+  		return idoso->cuidadores;
 }
 
-
+//  Retona a latitude do idoso
+//  inputs: ponteiro para a estrutura Idoso
+//  output: numero real type float
+//  pre-condicao: idoso precisa existir
+//  pos-condicao: idoso nao é modificado e latitude disponível
 float getLatitudeIdoso(Idoso *idoso){
-	return idoso->latitude;
+
+	// Se idoso não existir, retorna sinal de erro (-1)
+	if (idoso == NULL){
+		return ERRO_signal;
+	}
+	else
+		return idoso->latitude;
 }
 
+//  Retona a longitude do idoso
+//  inputs: ponteiro para a estrutura Idoso
+//  output: numero real type float
+//  pre-condicao: idoso precisa existir
+//  pos-condicao: idoso nao é modificado e longitude disponível
+//
 float getLongitudeIdoso(Idoso *idoso){
-	return idoso->longitude;
+
+	// Se idoso não existir, retorna sinal de erro (-1)
+	if (idoso == NULL){
+		return ERRO_signal;
+	}
+	else
+		return idoso->longitude;
 }
 
+//  Retona a temperatura do idoso
+//  inputs: ponteiro para a estrutura Idoso
+//  output: temperatura, um numero real type float
+//  pre-condicao: idoso precisa existir
+//  pos-condicao: idoso nao é modificado e temperatura disponível
+//
 float getTemperaturaIdoso(Idoso* idoso){
-	return idoso->temperatura;
+
+	// Se idoso não existir, retorna sinal de erro (-1)
+	if (idoso == NULL){
+		return ERRO_signal;
+	}
+	else
+		return idoso->temperatura;
 }
 
-int getQuedaIdoso(Idoso* idoso){
-	return idoso->queda;
+
+
+//  Retona informacao de queda do idoso
+//  inputs: ponteiro para a estrutura Idoso
+//  output: boolean que indica se idoso sofreu queda
+//  pre-condicao: idoso precisa existir
+//  pos-condicao: idoso nao é modificado e 'informacao de queda' disponível
+//
+bool getQuedaIdoso(Idoso* idoso){
+
+	// Se idoso não existir, retorna sinal de erro (-1)
+	if (idoso == NULL){
+		return ERRO_signal;
+	}
+	else
+		return idoso->queda;
 }
 
+//  Retona a quantidade de febres baixas do idoso
+//  inputs: ponteiro para a estrutura Idoso
+//  output: inteiro com a quantidade de febres baixas
+//  pre-condicao: idoso precisa existir
+//  pos-condicao: idoso nao é modificado e quantidade de febres disponível
+//
 int getNumFebresBaixas(Idoso* idoso){
-	return idoso->numFebresBaixas;
+
+	// Se idoso não existir, retorna sinal de erro (-1)
+	if (idoso == NULL){
+		return ERRO_signal;
+	}
+	else
+		return idoso->numFebresBaixas;
 }
 
 
@@ -91,40 +204,133 @@ int getNumFebresBaixas(Idoso* idoso){
  * 
  */
 
+//  Modifica a Latitude do Idoso
+//  inputs: 
+//		- Idoso* idoso = ponteiro para a estrutura Idoso
+//		- float lat = latitude do idoso, do tipo float
+//
+//  output: nada
+//  pre-condicao: idoso precisa existir e latituda positiva
+//  pos-condicao: latitude do idoso modificada
+//
 void setLatitudeIdoso(Idoso *idoso, float lat){
-	idoso->latitude = lat;
+
+	// Se idoso existir e lat não for negativo, modifica a latitude
+	if (idoso != NULL && lat >= 0)
+		idoso->latitude = lat;
 }
 
+
+
+//  Modifica a Longitude do Idoso
+//  inputs: 
+//		- Idoso* idoso = ponteiro para a estrutura Idoso
+//		- float longit = longitude do idoso, do tipo float
+//
+//  output: nada
+//  pre-condicao: idoso precisa existir e longitude não negativa
+//  pos-condicao: logitude do idoso modificada
+//
 void setLongitudeIdoso(Idoso *idoso, float longit){
-	idoso->longitude = longit;
+
+	// Se idoso existir e longit não for negativo, modifica a latitude
+	if (idoso != NULL && longit >= 0)
+		idoso->longitude = longit;
 }
 
+
+//  Modifica a Temperatura do Idoso
+//  inputs: 
+//		- Idoso* idoso = ponteiro para a estrutura Idoso
+//		- float temp = temperatura do idoso, do tipo float
+//
+//  output: nada
+//  pre-condicao: idoso precisa existir e temperatura positiva
+//  pos-condicao: temperatura do idoso modificada
+//
 void setTemperaturaIdoso(Idoso* idoso, float temp){
-	idoso->temperatura = temp;
+
+	if (idoso != NULL && temp >= 0)
+		idoso->temperatura = temp;
 }
 
-void setQuedaIdoso(Idoso* idoso, int queda){
+//  Define o valor de queda do Idoso
+//  inputs: 
+//		- Idoso* idoso = ponteiro para a estrutura Idoso
+//		- bool queda = true ou false, indica se houve queda
+//
+//  output: nada
+//  pre-condicao: idoso precisa existir
+//  pos-condicao: queda do idoso modificada
+//
+void setQuedaIdoso(Idoso* idoso, bool queda){
+
+	if (idoso != NULL)
 	idoso->queda = queda;
 }
 
+//  Define o valor de queda do Idoso
+//  inputs: 
+//		- Idoso* idoso = ponteiro para a estrutura Idoso
+//		- int febres = quantidade de febres baixas
+//
+//  output: nada
+//  pre-condicao: idoso precisa existir e febres >= 0
+//  pos-condicao: quantidade de febres do idoso modificada
+//
 void setNumFebresBaixas(Idoso* idoso, int febres){
+
+	if (idoso != NULL && febres >= 0)
 	idoso->numFebresBaixas = febres;
 }
 
+/*  Incrementa o valor de queda do Idoso
+ *  Inputs: 
+ *		- Idoso* idoso = ponteiro para a estrutura Idoso
+ * 
+ *  pre-condicao: idoso precisa existir
+ *  pos-condicao: quantidade de febres baixas do idoso incrementada em 1.
+ */
+void increaseNumFebresBaixas(Idoso *idoso){
 
-void destroiIdoso(Idoso *idoso){
-  free(idoso->nome);
-  //destroiListCuidadores(idoso->cuidadores);
-  //destroiListaAmigos(idoso->amigos);
-
-  free(idoso);
+	if (idoso != NULL)
+		idoso->numFebresBaixas++;
 }
 
+//  Libera memória alocada para o idoso
+//  inputs: ponteiro para a estrutura Idoso
+//  output:  0	->	tudo certo
+//			-1	->	idoso não existe
+//  pre-condicao: idoso existe
+//  pos-condicao: a memória alocada para a estrutura Idoso foi liberada
+//
+int destroiIdoso(Idoso *idoso){
+
+	// Se Idoso não existe, retorna um código de erro
+	if (idoso == NULL){
+		return -1;
+	}
+	// Se existir, libera a memoria
+	else{ 
+		free(idoso->nome);
+  		free(idoso->cuidadores);
+  		free(idoso->amigos);
+  		free(idoso);
+	}
+}
+
+//  Imprime idoso
+//  inputs: ponteiro para Usuario
+//  output: nenhum
+//  pre-condicao: Usuario precisa existir
+//  pos-condicao: Usuario nao eh alterado
+//
 void imprimeIdoso(Idoso* idoso){
-  printf("Nome: %s\n", idoso->nome);
-  //imprimeListCuidadores(idoso->cuidadores);
-  imprimeListAmigos(idoso->amigos);
-  printf("%f, %f, %f, %i", idoso->temperatura, idoso->latitude, idoso->longitude, idoso->queda);
+
+	printf("Nome: %s\n", idoso->nome);
+	imprimeListCuidador(idoso->cuidadores);
+	imprimeListAmigos(idoso->amigos);
+	printf("%f, %f, %f, %d", idoso->temperatura, idoso->latitude, idoso->longitude, idoso->queda);
 }
 
 
--- Source/Idoso.h
+++ Source/Idoso.h
@@ -1,7 +1,11 @@
 #ifndef IDOSO_H_
 #define IDOSO_H_
 
+#include <stdbool.h>
+
 typedef struct idoso Idoso;
+
+
 
 #include "ListaCuidadores.h"
 #include "ListaIdoso.h"
@@ -20,14 +24,14 @@ Idoso *criaIdoso(char *nome);
  *
  * FUNCOES GET - RETORNAM UM ATRIBUTO DO IDOSO
  * 
- */
+ *****************************************************/
 
 //  Retorna o nome do idoso
 //  inputs: ponteiro para a estrutura Idoso
 //  output: nome do idoso
 //  pre-condicao: idoso existe
 //  pos-condicao: idoso nao é modificado e nome do idoso disponível
-
+//
 char *getNomeIdoso(Idoso *idoso);
 
 
@@ -36,6 +40,7 @@ char *getNomeIdoso(Idoso *idoso);
 //  output: ponteiro para estrutura ListAmigos
 //  pre-condicao: idoso precisa existir
 //  pos-condicao: idoso nao é alterado e lista de amigos disponível
+//
 ListAmigos *getListAmigosIdoso(Idoso *idoso);
 
 
@@ -44,60 +49,137 @@ ListAmigos *getListAmigosIdoso(Idoso *idoso);
 //  output: ponteiro para a estrutura ListCuidadores
 //  pre-condicao: idoso precisa existir
 //  pos-condicao: idoso nao é modificado e estrutura dos cuidadores disponível
-
+//
 ListCuidador *getListCuidadores(Idoso *idoso);
 
 
-
+//  Retona a latitude do idoso
+//  inputs: ponteiro para a estrutura Idoso
+//  output: numero real type float
+//  pre-condicao: idoso precisa existir
+//  pos-condicao: idoso nao é modificado e latitude disponível
+//
 float getLatitudeIdoso(Idoso *idoso);
 
+//  Retona a longitude do idoso
+//  inputs: ponteiro para a estrutura Idoso
+//  output: numero real type float
+//  pre-condicao: idoso precisa existir
+//  pos-condicao: idoso nao é modificado e longitude disponível
+//
 float getLongitudeIdoso(Idoso *idoso);
 
+//  Retona a temperatura do idoso
+//  inputs: ponteiro para a estrutura Idoso
+//  output: temperatura, um numero real type float
+//  pre-condicao: idoso precisa existir
+//  pos-condicao: idoso nao é modificado e temperatura disponível
+//
 float getTemperaturaIdoso(Idoso* idoso);
 
-int getQuedaIdoso(Idoso* idoso);
+//  Retona informacao de queda do idoso
+//  inputs: ponteiro para a estrutura Idoso
+//  output: boolean que indica se idoso sofreu queda
+//  pre-condicao: idoso precisa existir
+//  pos-condicao: idoso nao é modificado e 'informacao de queda' disponível
+//
+bool getQuedaIdoso(Idoso* idoso);
 
+//  Retona a quantidade de febres baixas do idoso
+//  inputs: ponteiro para a estrutura Idoso
+//  output: inteiro com a quantidade de febres baixas
+//  pre-condicao: idoso precisa existir
+//  pos-condicao: idoso nao é modificado e quantidade de febres disponível
+//
 int getNumFebresBaixas(Idoso* idoso);
-
-
-/******************************************************************************/
-
-
 
 
 /*****************************************************
  *
  * FUNCOES SET - MODIFICAM UM ATRIBUTO DO IDOSO
  * 
- */
+ *****************************************************/
 
+//  Modifica a Latitude do Idoso
+//  inputs: 
+//		- Idoso* idoso = ponteiro para a estrutura Idoso
+//		- float lat = latitude do idoso, do tipo float
+//
+//  output: nada
+//  pre-condicao: idoso precisa existir e latituda positiva
+//  pos-condicao: latitude do idoso modificada
+//
 void setLatitudeIdoso(Idoso *idoso, float lat);
 
+//  Modifica a Longitude do Idoso
+//  inputs: 
+//		- Idoso* idoso = ponteiro para a estrutura Idoso
+//		- float longit = longitude do idoso, do tipo float
+//
+//  output: nada
+//  pre-condicao: idoso precisa existir e longitude não negativa
+//  pos-condicao: logitude do idoso modificada
+//
 void setLongitudeIdoso(Idoso *idoso, float longit);
 
+
+//  Modifica a Temperatura do Idoso
+//  inputs: 
+//		- Idoso* idoso = ponteiro para a estrutura Idoso
+//		- float temp = temperatura do idoso, do tipo float
+//
+//  output: nada
+//  pre-condicao: idoso precisa existir e temperatura positiva
+//  pos-condicao: temperatura do idoso modificada
+//
 void setTemperaturaIdoso(Idoso* idoso, float temp);
 
-void setQuedaIdoso(Idoso* idoso, int queda);
+//  Define o valor de queda do Idoso
+//  inputs: 
+//		- Idoso* idoso = ponteiro para a estrutura Idoso
+//		- bool queda = true ou false, indica se houve queda
+//
+//  output: nada
+//  pre-condicao: idoso precisa existir
+//  pos-condicao: queda do idoso modificada
+//
+void setQuedaIdoso(Idoso* idoso, bool queda);
 
+//  Define o valor de queda do Idoso
+//  inputs: 
+//		- Idoso* idoso = ponteiro para a estrutura Idoso
+//		- int febres = quantidade de febres baixas
+//
+//  output: nada
+//  pre-condicao: idoso precisa existir e febres >= 0
+//  pos-condicao: quantidade de febres do idoso modificada
+//
 void setNumFebresBaixas(Idoso* idoso, int febres);
 
 
+/*****************************************************************************
+ * 
+ * OUTRAS FUNÇÕES
+ * 
+ *****************************************************************************/
 
-
-
-
-
-
+/*  Incrementa o valor de queda do Idoso
+ *  Inputs: 
+ *		- Idoso* idoso = ponteiro para a estrutura Idoso
+ * 
+ *  pre-condicao: idoso precisa existir
+ *  pos-condicao: quantidade de febres baixas do idoso incrementada em 1.
+ */
+void increaseNumFebresBaixas(Idoso *idoso);
 
 //  Libera memória alocada para o idoso
 //  inputs: ponteiro para a estrutura Idoso
-//  output: nenhum
+//  output:  0	->	tudo certo
+//			-1	->	idoso não existe
 //  pre-condicao: idoso existe
 //  pos-condicao: a memória alocada para a estrutura Idoso foi liberada
 //
-void destroiIdoso(Idoso *idoso);
-
-
+int destroiIdoso(Idoso *idoso);
 
 //  Imprime idoso
 //  inputs: ponteiro para Usuario
@@ -108,13 +190,6 @@ void imprimeIdoso(Idoso* usuario);
 
 
 // Outras funcoes
-
-void LeLinhaCuidadorNoIdoso(char* arquivo, ListIdoso* listaIdoso, ListCuidador* listaCuidador);
-
-void InsereCuidadorNoIdoso(Idoso* idoso, Cuidador* cuidador);
-
-void InsereMedidasIdoso(Idoso* idoso, int linhaArquivo, char* arquivo);
-
 
 
 
