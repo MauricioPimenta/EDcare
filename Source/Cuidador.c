a8abdaf3d9bb4bbe5503088a7597beac65d9ae16
douglastein <86307590+douglastein@users.noreply.github.com>
2022-02-17 03:57:32 -0300

    Update Cuidador.c

--- Source/Cuidador.c
+++ Source/Cuidador.c
@@ -1,18 +1,11 @@
-/*
- * INCLUSAO DE BIBLIOTECAS
- */
-
-// BIBLIOTECAS PADRÃO
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// BIBLIOTECAS DO USUÁRIO
 #include "Cuidador.h"
 
-/*
- * Definição da Estrutura do TAD Cuidador
- */
+
+
 struct cuidador{
   char *nome;
   float Latitude;
@@ -33,99 +26,79 @@ Cuidador* criaCuidador(char* nome){
   return cuidador;
 }
 
-
-
-/*****************************************************
- *
- * FUNCOES GET - RETORNAM UM ATRIBUTO DO CUIDADOR
- *
- *****************************************************/
-
-/*
- * Retorna o Nome do Cuidador
- */
-char* getNomeCuidador(Cuidador* cuidador){
-  if(cuidador == NULL){  //se nao existe, retorna nada
-    return NULL;
-  }
+char* retornaNomeCuidador(Cuidador* cuidador){
   return cuidador->nome;
 }
 
-/*
- * Retorna a Latitude do Cuidador
- */
-int getLatitudeCuidador(Cuidador* cuidador){
-  if(cuidador == NULL){  //se nao existe, retorna nada
-    return NULL;
-  }
+float retornaLatitudeCuidador(Cuidador* cuidador){
   return cuidador->Latitude;
 }
 
-/*
- * Retorna a Longitude do Cuidador
- */
-int getLongitudeCuidador(Cuidador* cuidador){
-  if(cuidador == NULL){  //se nao existe, retorna nada
-    return NULL;
-  }
+float retornaLongitudeCuidador(Cuidador* cuidador){
   return cuidador->Longitude;
 }
 
+void InsereMedidasCuidador(Cuidador* cuidador, int linhaArquivo, char* arquivo){
 
-/*****************************************************
- *
- * FUNCOES SET - MODIFICAM UM ATRIBUTO DO IDOSO
- *
- ****************************************************/
+    FILE *fp = fopen(arquivo, "r");
 
-/*
- * Modifca a Latitude do Cuidador
- */
-void setLatitudeCuidador(Cuidador* c, float lat){
-    if(cuidador == NULL){  //se nao existe, retorna nada
+    if(fp == NULL){
+        printf("Erro no arquivo de medidas de %s\n.", retornaNomeCuidador(cuidador));
         return NULL;
     }
-	c->Latitude = lat;
-}
 
-/*
- * Modifca a Longitude do Cuidador
- */
-void setLongitudeCuidador(Cuidador* c, float l){
-    if(cuidador == NULL){  //se nao existe, retorna nada
-        return NULL;
+    if(linhaArquivo == 1){
+        char linha[1000];
+        fscanf(fp, "%[^\n]\n", linha);  //le a linha de interesse
+        char* medida = strtok(linha, ";");  //separa as medidas da linha pelo separador ";"
+
+        int k = 0;  //auxilia na inserção das medidas nos campos corretos através das condicoes
+        while(medida != NULL){
+            k++;
+            if(k == 1){
+                cuidador->Latitude = atof(medida);
+            }
+            else{
+                cuidador->Longitude = atof(medida);
+            }
+
+            medida = strtok(NULL, ";");
+        }
     }
-	c->Longitude = l;
+    else{
+        char linha[1000];
+        int i;
+        for(i = 0; i < linhaArquivo - 1; i++){
+            fscanf(fp, "%[^\n]\n", linha);  //le até a ultima linha antes da linha de interesse
+        }
+
+        fscanf(fp, "%[^\n]\n", linha);  //le a linha de interesse
+        char* medida = strtok(linha, ";");  //separa as medidas da linha pelo separador ";"
+
+        int k = 0;  //auxilia na inserção das medidas nos campos corretos através das condicoes
+        while(medida != NULL){
+            k = k + 1;
+            if(k == 1){
+                cuidador->Latitude = atof(medida);
+            }
+            else{
+                cuidador->Longitude = atof(medida);
+            }
+
+            medida = strtok(NULL, ";");
+        }
+    }
+
+    fclose(fp);
 }
 
-
-
-
-//  Funcao que libera memoria alocada para o cuidador
-//  inputs: ponteiro para a estrutura cuidador
-//  output: nenhum
-//  pre-condicao: Cuidador existe
-//  pos-condicao: toda a memoria alocada para cuidador foi liberada
 void destroiCuidador(Cuidador* cuidador){
-  if(cuidador == NULL){  //se nao existe, retorna nada
-    return NULL;
-  }
   free(cuidador->nome);
   free(cuidador);
 }
 
-
-//  Imprime cuidador
-//  inputs: ponteiro para cuidador
-//  output: nenhum
-//  pre-condicao: cuidador existe
-//  pos-condicao: cuidador nao e alterado
 void imprimeCuidador(Cuidador* cuidador){
-  if(cuidador == NULL){  //se nao existe, retorna nada
-    return NULL;
-  }
   printf("\nNome: %s\n", cuidador->nome);
   printf("Posicao: %0.2f,%0.2f\n\n", cuidador->Latitude, cuidador->Longitude);
-
 }
 
